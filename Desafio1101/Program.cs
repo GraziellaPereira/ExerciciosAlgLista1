d7ddb7202b9c7a098c379fb0e205cc0c75333102
GraziellaPereira <graziellapereira52@gmail.com>
2025-03-01 23:54:11 -0300

    Lembrete: terminar desafio 1101

---
+++ Desafio1101/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1099
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois pares de números, até um chegar no 0: ");
+            string[] pares = Console.ReadLine().Split();
+            double m = double.Parse(pares[0]);
+            double n = double.Parse(pares[1]);
+
+            if (m > n)
+            {
+                double newM;
+                newM = n;
+                n = m;
+                m = newM;
+            }
+
+            while (n != 0)
+            {
+
+                for (double i = m; i <= n; i++)
+                {
+                    double[] sequencia = new double[i];
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercicio1133/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1071
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x, y, soma = 0;
+
+            Console.WriteLine("Digite dois números inteiros: ");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+            if (x > y)
+            {
+                int newX;
+                newX = y;
+                y = x;
+                x = newX;
+            }
+
+            for (int i = x + 1; i < y; i++)
+            {
+                if (i % 5 == 2 || i % 5 == 3)
+                {
+                    Console.WriteLine(i);
+
+                }
+
+            }
+            Console.WriteLine($"Soma dos números entre {x} e {y} (incluindo os mesmos) que não são divisíveis por 13 é igual a: {soma}");
+            Console.ReadKey();
+        }
+    }
+}
